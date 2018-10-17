7d94639f12f7cfb5acfe87b0ac8575d00289c45e
Peter Csajtai <peter.csajtai@outlook.com>
2018-10-17 17:33:07 +0200

    Modified message handler

--- extensions/http/TrybotMessageHandler.cs
+++ extensions/http/TrybotMessageHandler.cs
@@ -7,19 +7,22 @@ namespace Trybot.Extensions.Http
 {
     public class TrybotMessageHandler : DelegatingHandler
     {
-        private readonly IBotPolicy<HttpResponseMessage> botPolicy;
+        protected readonly IBotPolicy<HttpResponseMessage> BotPolicy;
 
         public TrybotMessageHandler(Action<IBotPolicyBuilder<HttpResponseMessage>> configuratorAction)
         {
-            this.botPolicy = new BotPolicy<HttpResponseMessage>();
-            this.botPolicy.Configure(configuratorAction);
+            this.BotPolicy = new BotPolicy<HttpResponseMessage>();
+            this.BotPolicy.Configure(configuratorAction);
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var correlationdId = request.GetCorrelationId();
-            return await this.botPolicy.ExecuteAsync((req, ctx, token) =>
-                base.SendAsync(req, token), correlationdId, request, cancellationToken);
+            return await this.BotPolicy.ExecuteAsync((req, ctx, token) =>
+                this.WrappedSendAsync(req, token), correlationdId, request, cancellationToken);
         }
+
+        protected virtual Task<HttpResponseMessage> WrappedSendAsync(HttpRequestMessage request, CancellationToken cancellationToken) =>
+            base.SendAsync(request, cancellationToken);
     }
 }
